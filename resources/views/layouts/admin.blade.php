68810fe893eaec09177adf3758f290fdbe964fc2
Bendexu <144471737+Bendexu@users.noreply.github.com>
2025-08-03 22:22:22 -0500

    dashboard
    dsad

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,8 +16,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons CDN -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<!-- dashboard -->
+<!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/fontawesome/css/all.min.css') }}">
 
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/summernote/summernote-bs4.css') }}">
 
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/components.css') }}">
+<!-- dashboard -->
     @vite(['resources/css/admin.css'])
   </head>
   <body>
@@ -224,6 +237,31 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+<!-- dashboard -->
+ <!-- General JS Scripts -->
+  <script src="{{ asset('backend/assets/modules/jquery.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/popper.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/tooltip.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/moment.min.j') }}s"></script>
+  <script src="{{ asset('backend/assets/js/stisla.js') }}"></script>
+  
+  <!-- JS Libraies -->
+  <script src="{{ asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/chart.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/summernote/summernote-bs4.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
+  <!-- Page Specific JS File -->
+  <script src="{{ asset('backend/assets/js/page/index-0.js') }}"></script>
+  
+  <!-- Template JS File -->
+  <script src="{{ asset('backend/assets/js/scripts.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/custom.js') }}"></script>
+  <!-- dashboard -->
     <!-- JavaScript -->
     <script>
       const body = document.querySelector("body");

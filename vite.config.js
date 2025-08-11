1d5a9de3cdb98e35a73fa3ac4cc7d7431e947fde
josuee-lang <juradoguevarajosue@gmail.com>
2025-08-11 04:12:27 -0500

    sda

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    base: "", // <-- aseguramos base relativa
     plugins: [
         laravel({
             input: ["resources/sass/app.scss", "resources/js/app.js"],
-            refresh: true, // solo útil en desarrollo
+            refresh: true,
         }),
     ],
     build: {

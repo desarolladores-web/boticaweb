fdbf8b9a4cce3d71e2e2431d6c230fef4cb219bd
josuee-lang <juradoguevarajosue@gmail.com>
2025-08-11 04:13:50 -0500

    asd

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    base: "",
     plugins: [
         laravel({
             input: ["resources/sass/app.scss", "resources/js/app.js"],
@@ -12,5 +13,12 @@ export default defineConfig({
         outDir: "public/build",
         manifest: true,
         emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: "assets/[name]-[hash].js",
+                chunkFileNames: "assets/[name]-[hash].js",
+                assetFileNames: "assets/[name]-[hash][extname]",
+            },
+        },
     },
 });

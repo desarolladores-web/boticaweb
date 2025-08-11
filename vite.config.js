b003ba47743c8ef309381808274a01fbf7ee0ccf
josuee-lang <juradoguevarajosue@gmail.com>
2025-08-11 04:43:10 -0500

    A

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
+            useBuildDirectory: false, // Evita la carpeta .vite
         }),
     ],
     build: {

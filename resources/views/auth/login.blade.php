b5cd90c41068130e0d00f21db689117ccfbcefcc
josuee-lang <juradoguevarajosue@gmail.com>
2025-12-03 00:46:56 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,13 +7,14 @@
         <div class="card shadow-lg border-0 rounded-4 overflow-hidden">
             <div class="row g-0 align-items-center">
 
-                  <!-- Left side: Imagen y bienvenida -->
-            <div class="login-right">
-                <img src="{{ asset('imagenes/login.png') }}" alt="Welcome Image" style="width: 400px; margin-bottom: 20px;">
-                <h2>¿Eres nuevo aquí?</h2>
-                <p>¡Regístrate y descubre una gran cantidad de nuevas oportunidades!</p>
-                <a href="{{ route('register') }}" class="btn">Registrarse</a>
-            </div>
+                <!-- Left side: Imagen y bienvenida -->
+                <div class="login-right">
+                    <img src="{{ asset('imagenes/login.png') }}" alt="Welcome Image"
+                        style="width: 400px; margin-bottom: 20px;">
+                    <h2>¿Eres nuevo aquí?</h2>
+                    <p>¡Regístrate y descubre una gran cantidad de nuevas oportunidades!</p>
+                    <a href="{{ route('register') }}" class="btn">Registrarse</a>
+                </div>
                 <!-- Right side: login form -->
                 <div class="col-12 col-md-6 p-4">
                     <h1 class="text-center mb-4">Iniciar sesión en su cuenta</h1>
@@ -27,10 +28,9 @@
                             <label for="email" class="form-label">
                                 <i class="bi bi-person me-2"></i> {{ __('Ingresa tus datos') }}
                             </label>
-                            <input id="email" type="email"
-                                class="form-control @error('email') is-invalid @enderror"
-                                placeholder="Correo" name="email" value="{{ old('email') }}" required autocomplete="email"
-                                autofocus>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                placeholder="Correo" name="email" value="{{ old('email') }}" required
+                                autocomplete="email" autofocus>
                             @error('email')
                                 <span class="invalid-feedback d-block">
                                     <strong>{{ $message }}</strong>
@@ -66,7 +66,7 @@
 
                         @if (Route::has('password.request'))
                             <a class="btn btn-link mt-3 d-block text-center" href="{{ route('password.request') }}">
-                                {{ __('¿Olvidaste tu contraseña?') }}
+                                <!--  {{ __('¿Olvidaste tu contraseña?') }} -->
                             </a>
                         @endif
                     </form>
